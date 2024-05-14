94587946febb0af2b8331b21349b0edc9cc9883c
Dmytro-Andrii Kostelnyi <dmitrokostelny95@gmail.com>
2024-05-14 19:42:08 +0300

    display map

---
+++ src/components/Map/index.tsx
@@ -0,0 +1,35 @@
+import React, { useRef, useEffect, useState } from 'react'
+
+import mapboxgl from 'mapbox-gl'
+
+import style from './Map.module.css'
+import '~mapbox-gl-style'
+
+mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN
+
+
+function Map() {
+  const map = useRef(null)
+  const mapContainer = useRef(null)
+  const [lng, setLng] = useState(-70.9)
+  const [lat, setLat] = useState(42.35)
+  const [zoom, setZoom] = useState(9)
+
+  useEffect(() => {
+    if (map.current) return
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: [lng, lat],
+      zoom: zoom
+    })
+  }, [])
+
+  return (
+    <div>
+      <div ref={mapContainer} className={style.container} />
+    </div>
+  )
+}
+
+export default Map
