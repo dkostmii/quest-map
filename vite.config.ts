58b848ceaebf92d48ba15a6f7f5f0fb899c38a47
Dmytro-Andrii Kostelnyi <dmitrokostelny95@gmail.com>
2024-05-14 19:41:48 +0300

    config project
    Use aliases for common paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
+import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '~mapbox-gl-style': path.resolve(__dirname, 'node_modules/mapbox-gl/dist/mapbox-gl.css')
+    }
+  }
 })
